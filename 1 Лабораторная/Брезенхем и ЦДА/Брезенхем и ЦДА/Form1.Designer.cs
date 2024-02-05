d4709ebf621fc8ddf4fea34605968d84834b204b
Wuffluv <rodion.rycha@yandex.ru>
2024-02-06 00:44:51 +0900

    Продолжаю работу

---
+++ 1 Лабораторная/Брезенхем и ЦДА/Брезенхем и ЦДА/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Брезенхем_и_ЦДА
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 343);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Введите X1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 380);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Введите Y1";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 262);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(587, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(315, 262);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(103, 458);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(245, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Введите Y2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(103, 419);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(245, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "Введите X2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Алгоритм Брезенхема";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(718, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Метод ЦДА";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(638, 343);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(245, 22);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "Введите X1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(638, 385);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(245, 22);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.Text = "Введите Y1";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(638, 424);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(245, 22);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.Text = "Введите X2";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(638, 462);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(245, 22);
+            this.textBox8.TabIndex = 12;
+            this.textBox8.Text = "Введите Y2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(965, 544);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+
+}
+
---
+++ 1 Лабораторная/Брезенхем и ЦДА/Брезенхем и ЦДА/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Брезенхем_и_ЦДА
+{
+    public partial class Form1 : Form
+    {
+        // Переменные для хранения координат
+        private int x1, y1, x2, y2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для pictureBox1 (алгоритм Брезенхема)
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // При нажатии кнопки "Нарисовать"
+
+            // Парсим введенные координаты
+            if (int.TryParse(textBox1.Text, out x1) && int.TryParse(textBox2.Text, out y1) &&
+                int.TryParse(textBox3.Text, out x2) && int.TryParse(textBox4.Text, out y2))
+            {
+                // Рисуем линию по алгоритму Брезенхема в PictureBox1
+                DrawBresenhamLine();
+
+                // Рисуем линию по методу ЦДА в PictureBox2
+                DrawDDALine();
+            }
+            else
+            {
+                // Выводим сообщение об ошибке в случае некорректных координат
+                MessageBox.Show("Введите корректные координаты");
+            }
+        }
+
+        private void DrawBresenhamLine()
+        {
+            // Рисуем линию по алгоритму Брезенхема
+
+            // Создаем новый объект Bitmap для хранения изображения
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                // Вызываем метод для рисования линии по алгоритму Брезенхема
+                BresenhamLine(x1, y1, x2, y2, g);
+            }
+            // Отображаем изображение в pictureBox1
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawDDALine()
+        {
+            // Рисуем линию по методу ЦДА
+
+            // Создаем новый объект Bitmap для хранения изображения
+            Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                // Вызываем метод для рисования линии по методу ЦДА
+                DDALine(x1, y1, x2, y2, g);
+            }
+            // Отображаем изображение в pictureBox2
+            pictureBox2.Image = bmp;
+        }
+
+        private void BresenhamLine(int x1, int y1, int x2, int y2, Graphics g)
+        {
+            // Реализация алгоритма Брезенхема для рисования линии
+
+            // Вычисляем разницу по x и y
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+
+            // Устанавливаем направление отрисовки по x и y
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+
+            // Вычисляем ошибку
+            int err = dx - dy;
+
+            // Основной цикл отрисовки
+            while (true)
+            {
+                // Закрашиваем текущий пиксель
+                g.FillRectangle(Brushes.Black, x1, y1, 1, 1);
+
+                // Если достигли конечной точки, выходим из цикла
+                if (x1 == x2 && y1 == y2)
+                    break;
+
+                // Вычисляем ошибку для следующего шага
+                int e2 = 2 * err;
+
+                // Перемещаемся по оси x при необходимости
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+
+                // Перемещаемся по оси y при необходимости
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DDALine(int x1, int y1, int x2, int y2, Graphics g)
+        {
+            // Реализация метода ЦДА для рисования линии
+
+            // Вычисляем разницу по x и y
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            // Находим максимальное количество шагов
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            // Вычисляем инкременты по x и y
+            float xIncrement = dx / (float)steps;
+            float yIncrement = dy / (float)steps;
+
+            // Инициализируем начальные значения
+            float x = x1;
+            float y = y1;
+
+            // Основной цикл отрисовки
+            for (int i = 0; i <= steps; i++)
+            {
+                // Закрашиваем текущий пиксель
+                g.FillRectangle(Brushes.Black, (int)x, (int)y, 1, 1);
+
+                // Обновляем координаты для следующего шага
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Обработчик события изменения текста в textBox2
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            // Обработчик события для pictureBox2 (метод ЦДА)
+        }
+    }
+}
