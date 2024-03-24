89972d16139afc62fe720c74495620bf16e911df
Wuffluv <rodion.rycha@yandex.ru>
2024-03-24 22:15:56 +0900

    Вроде получилось

--- 5-я лабораторная/Paint/Paint/Form1.Designer.cs
+++ 5-я лабораторная/Paint/Paint/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 294);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(485, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выбирать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(485, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выбрать цвет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(693, 348);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- 5-я лабораторная/Paint/Paint/Form1.cs
+++ 5-я лабораторная/Paint/Paint/Form1.cs
@@ -1,20 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Paint
 {
     public partial class Form1 : Form
     {
+        // Перечисление для определения текущего действия мыши
+        private enum MouseAction { Draw, Fill };
+
+        // Текущее действие мыши и цвет по умолчанию
+        private MouseAction currentAction = MouseAction.Draw;
+        private Color currentColor = Color.Black;
+
+        // Точка начала рисования, флаг рисования и графика для рисования
+        private Point startPoint;
+        private bool drawing = false;
+        private Graphics graphics;
+        private Bitmap bitmap;
+
         public Form1()
         {
             InitializeComponent();
+            InitializePictureBox();
+        }
+
+        // Инициализация PictureBox и привязка обработчиков событий
+        private void InitializePictureBox()
+        {
+            // Создание нового изображения для PictureBox
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // Получение объекта Graphics для рисования на изображении
+            graphics = Graphics.FromImage(bitmap);
+
+            // Установка изображения для PictureBox и очистка его фона
+            pictureBox1.Image = bitmap;
+            graphics.Clear(Color.White);
+
+            // Привязка обработчиков событий мыши
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+        }
+
+        // Обработчик события отпускания кнопки мыши
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false; // Остановка рисования
+        }
+
+        // Обработчик события нажатия кнопки мыши
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (currentAction == MouseAction.Draw)
+            {
+                drawing = true; // Начало рисования
+                startPoint = e.Location; // Запоминаем начальную точку
+            }
+            else if (currentAction == MouseAction.Fill)
+            {
+                FillRegion(e.Location, currentColor); // Заполнение области цветом
+            }
+        }
+
+        // Обработчик события перемещения мыши
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Если идет рисование и выбрано действие "рисование"
+            if (drawing && currentAction == MouseAction.Draw)
+            {
+                // Создание пера для рисования линии
+                Pen pen = new Pen(currentColor, 2);
+
+                // Рисование линии между предыдущей и текущей точками
+                graphics.DrawLine(pen, startPoint, e.Location);
+
+                // Обновление начальной точки для следующей линии
+                startPoint = e.Location;
+
+                // Обновление PictureBox для отображения изменений
+                pictureBox1.Invalidate();
+            }
+        }
+
+        // Метод для заливки области цветом
+        private void FillRegion(Point startPoint, Color fillColor)
+        {
+            // Создание временного изображения для работы с пикселями
+            Bitmap tempBitmap = new Bitmap(bitmap);
+            Queue<Point> points = new Queue<Point>();
+            points.Enqueue(startPoint);
+
+            // Получение цвета начальной точки
+            Color targetColor = tempBitmap.GetPixel(startPoint.X, startPoint.Y);
+
+            // Поиск всех точек, которые должны быть заполнены новым цветом
+            while (points.Count > 0)
+            {
+                Point currentPoint = points.Dequeue();
+
+                // Проверка находится ли точка внутри границ PictureBox
+                if (currentPoint.X < 0 || currentPoint.Y < 0 || currentPoint.X >= pictureBox1.Width || currentPoint.Y >= pictureBox1.Height)
+                    continue;
+
+                // Если цвет текущей точки совпадает с начальным цветом
+                if (tempBitmap.GetPixel(currentPoint.X, currentPoint.Y) == targetColor)
+                {
+                    // Заполняем точку новым цветом
+                    tempBitmap.SetPixel(currentPoint.X, currentPoint.Y, fillColor);
+
+                    // Добавляем соседние точки для проверки их цвета
+                    points.Enqueue(new Point(currentPoint.X + 1, currentPoint.Y));
+                    points.Enqueue(new Point(currentPoint.X - 1, currentPoint.Y));
+                    points.Enqueue(new Point(currentPoint.X, currentPoint.Y + 1));
+                    points.Enqueue(new Point(currentPoint.X, currentPoint.Y - 1));
+                }
+            }
+
+            // Обновление изображения и PictureBox
+            bitmap = tempBitmap;
+            pictureBox1.Image = bitmap;
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentAction = MouseAction.Draw; 
+        }
+
+  
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentAction = MouseAction.Fill; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //диалог выбора цвета и установка текущего цвета
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentColor = colorDialog.Color;
+            }
         }
     }
 }
